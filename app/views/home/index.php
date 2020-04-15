b52c3ef60618fd342fff7d273ff7f79db8f44628
nhardiansa <44939789+nhardiansa@users.noreply.github.com>
2020-04-15 09:26:17 +0800

    Create index.php

---
+++ app/views/home/index.php
@@ -0,0 +1,9 @@
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="display-4">Homepage</h1>
+        <p class="lead">Ini halaman Home ya bujang</p>
+        <button type="button" class="btn btn-primary" onclick="alert('Hello World');">
+            Hello
+        </button>
+    </div>
+</div>
